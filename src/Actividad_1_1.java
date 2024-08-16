2ea4157bd23d58a2756595343abb0c4d62af7675
Joaquin <jmarcoff@uade.edu.ar>
2024-08-16 11:58:58 -0300

    Primer commit del proyecto

---
+++ src/Actividad_1_1.java
@@ -0,0 +1,21 @@
+public class Actividad_1_1 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 9, 0, 4, 3, 6, 12}; // 1
+
+        // variable max
+        int max = 0; // 1
+
+        // ciclo que se va quedando con el máximo y descartando los otros.
+        for (int j = 0; j < array.length; j++) { // 1+2(n+1)+n
+            if (array[j] > max) { //2n
+                max = array[j]; //n
+            }
+        }
+        // print resultado
+        System.out.println(max); // 1
+
+    }
+}
+
+//f(n)=1+1+1+2(n+1)+n+2n+n+1
+//f(n)=6+6n
