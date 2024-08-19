5e678a5f0ecf42267edf254cb2758296a728eb42
Joaquin <jmarcoff@uade.edu.ar>
2024-08-19 10:41:11 -0300

    Refactor stricture

--- src/Actividad_1_1_b/Cliente.java
+++
@@ -1,6 +0,0 @@
-package Actividad_1_1_b;
-
-public class Cliente {
-    int id_Cliente;
-    String nombre;
-}
--- src/Actividad_1_1_b/Factura.java
+++
@@ -1,7 +0,0 @@
-package Actividad_1_1_b;
-
-public class Factura {
-    int id_Cliente;
-    double importe;
-    int id_Factura;
-}
